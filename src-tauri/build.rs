7cbb1029c86436da29f89ff11275e683135c8db5
winterSteve25 <w1nter.ish3re@gmail.com>
2023-12-03 16:59:14 -0500

    fixed can not have await in top level code

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,7 @@
+use std::fs;
+
 fn main() {
+    fs::copy("../splashscreen.html", "../dist/splashscreen.html")
+        .expect("failed to copy over splashscreen.html");
     tauri_build::build()
 }
--- src/App.svelte
+++ src/App.svelte
@@ -2,22 +2,36 @@
     import {Pane, Splitpanes} from "svelte-splitpanes";
     import FileExplorer from "./lib/components/tree/FileExplorer.svelte";
     import Editor from "./lib/components/editor/Editor.svelte";
+
+    import {Settings} from "./lib/ts/settings";
+    import {Theming} from "./lib/ts/theme";
+    import {invoke} from "@tauri-apps/api";
+
+    async function load() {
+        await Theming.loadThemes();
+        await Settings.load();
+        await invoke("close_splashscreen");
+    }
 </script>
 
 <main>
-    <Splitpanes theme="custom">
-        <Pane minSize={15} snapSize={2} size={20}>
-            <FileExplorer/>
-        </Pane>
-        <Pane minSize={20}>
-            <Editor/>
-        </Pane>
-    </Splitpanes>
+    {#await load()}
+        Loading
+    {:then _}
+        <Splitpanes theme="custom">
+            <Pane minSize={15} snapSize={2} size={20}>
+                <FileExplorer/>
+            </Pane>
+            <Pane minSize={20}>
+                <Editor/>
+            </Pane>
+        </Splitpanes>
+    {/await}
 </main>
 
 <style lang="scss">
-  main {
-    height: 100%;
-    width: 100%;
-  }
+    main {
+        height: 100%;
+        width: 100%;
+    }
 </style>
