58a515e9c2893be0dcc205c5619308129c26ffa1
winterSteve25 <w1nter.ish3re@gmail.com>
2023-12-07 09:27:17 -0500

    creates the dist directory if not found

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,14 @@
 use std::fs;
+use std::path::Path;
 
 fn main() {
+    
+    if !Path::new("../dist").exists() {
+        fs::create_dir("../dist").expect("Failed to create dist directory");
+    }
+    
     fs::copy("../splashscreen.html", "../dist/splashscreen.html")
         .expect("failed to copy over splashscreen.html");
+    
     tauri_build::build()
 }
